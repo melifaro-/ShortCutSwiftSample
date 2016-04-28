9ff8ca81352c32ee514da4526d5655302c404664
botov <iebotov@gmail.com>
2016-04-28 15:20:05 +0300

    Better base key code setting

--- ShortcutSampleApp/ViewController.swift
+++ ShortcutSampleApp/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let shortcut = MASShortcut.init(keyCode: 40, modifierFlags: UInt(NSEventModifierFlags.CommandKeyMask.rawValue + NSEventModifierFlags.ShiftKeyMask.rawValue))
+        let shortcut = MASShortcut.init(keyCode: UInt(kVK_ANSI_K), modifierFlags: UInt(NSEventModifierFlags.CommandKeyMask.rawValue + NSEventModifierFlags.ShiftKeyMask.rawValue))
         
         MASShortcutMonitor.sharedMonitor().registerShortcut(shortcut, withAction: {
             print("Hello world")
