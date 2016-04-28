00bb0ef3ba9435f76b63f4544fc0a75d42dc7c28
botov <iebotov@gmail.com>
2016-04-28 17:45:18 +0300

    Updated per comments

--- ShortcutSampleApp/ViewController.swift
+++ ShortcutSampleApp/ViewController.swift
@@ -3,13 +3,27 @@ import AppKit
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var shortcutView: MASShortcutView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let shortcut = MASShortcut.init(keyCode: UInt(kVK_ANSI_K), modifierFlags: UInt(NSEventModifierFlags.CommandKeyMask.rawValue + NSEventModifierFlags.ShiftKeyMask.rawValue))
         
-        MASShortcutMonitor.sharedMonitor().registerShortcut(shortcut, withAction: {
-            print("Hello world")
-        })
+        shortcutView.shortcutValueChange = { (sender) in
+            
+            let callback: (() -> Void)!
+            
+            if self.shortcutView.shortcutValue.keyCodeStringForKeyEquivalent == "k" {
+                callback = {
+                    print("K shortcut handler")
+                }
+            } else {
+                callback = {
+                    print("Default handler")
+                }
+            }
+            
+            MASShortcutMonitor.sharedMonitor().registerShortcut(self.shortcutView.shortcutValue, withAction: callback)
+        }
+
     }
 }
