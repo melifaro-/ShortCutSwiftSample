96bcf0cb7cda870e3b8a062de0a528e90c05f004
botov <iebotov@gmail.com>
2016-05-01 14:33:57 +0300

    Unregistering shortcuts

--- ShortcutSampleApp/ViewController.swift
+++ ShortcutSampleApp/ViewController.swift
@@ -3,16 +3,22 @@ import AppKit
 
 class ViewController: NSViewController {
 
+    var kShortCut: MASShortcut!
+    
     @IBOutlet weak var shortcutView: MASShortcutView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
         shortcutView.shortcutValueChange = { (sender) in
             
             let callback: (() -> Void)!
             
             if self.shortcutView.shortcutValue.keyCodeStringForKeyEquivalent == "k" {
+                
+                self.kShortCut = self.shortcutView.shortcutValue
+                
                 callback = {
                     print("K shortcut handler")
                 }
@@ -24,6 +30,24 @@ class ViewController: NSViewController {
             
             MASShortcutMonitor.sharedMonitor().registerShortcut(self.shortcutView.shortcutValue, withAction: callback)
         }
-
+    }
+    
+    @IBAction func unregisterAllButtonClicked(sender: AnyObject) {
+        
+        MASShortcutMonitor.sharedMonitor().unregisterAllShortcuts()
+    }
+    
+    @IBAction func unregisterKShortCutButtonClicked(sender: AnyObject) {
+        
+        unregisterKShortCutIfNeeded()
+    }
+    
+    func unregisterKShortCutIfNeeded() {
+        
+        guard let shortcut = kShortCut else {
+            
+            return
+        }
+        MASShortcutMonitor.sharedMonitor().unregisterShortcut(shortcut)
     }
 }
